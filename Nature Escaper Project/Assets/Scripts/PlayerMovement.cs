e8bc1fbc5975719bb1a50060bb60b8cea11689eb
Euan Gordon <euangordon10@hotmail.com>
2021-04-26 10:28:37 +0100

    Added Animations

--- Nature Escaper Project/Assets/Scripts/PlayerMovement.cs
+++ Nature Escaper Project/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,25 @@ public class PlayerMovement : MonoBehaviour
     //public variables, visible in Unity inspector
     public float forceStrength;
 
+    //called once per frame
+    public void Update()
+    {
+        // Get the rigidbody from our player so we can check its speed
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        // Find out from the rigidbody what our current horizontal and vertical speeds are
+        float currentSpeedH = ourRigidbody.velocity.x;
+        float currentSpeedV = ourRigidbody.velocity.y;
+
+        // Get the animator that we'll be using for movement
+        Animator ourAnimator = GetComponent<Animator>();
+
+        // Tell our animator what the speeds are
+        ourAnimator.SetFloat("speedH", currentSpeedH);
+       
+
+    }
+
     //These will be called by each button for movement
     public void MoveLeft()
     {
