b189c079e88d1a25e90736785751c206f3a67a54
Euan Gordon <euangordon10@hotmail.com>
2021-05-17 00:40:23 +0100

    Added 2nd Level

---
+++ Nature Escaper Project/Assets/Scripts/FlagSceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlagSceneChange : MonoBehaviour
+{
+    public string levelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        //Debug.Log("Object hit the flag");
+
+        if (otherCollider.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+        
+    }
+}
