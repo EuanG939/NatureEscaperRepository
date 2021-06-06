6d7a60d16168a954a5d3c9167f4a3dcf0743b5ba
Euan Gordon <euangordon10@hotmail.com>
2021-06-06 23:43:31 +0100

    implemented custom assets and added score persistance

--- Nature Escaper Project/Assets/Scripts/Score.cs
+++ Nature Escaper Project/Assets/Scripts/Score.cs
@@ -7,9 +7,25 @@ public class Score : MonoBehaviour
 {
     //public variable
     public Text scoreDisplay;
+    public bool shouldReset = false;
     
     //private variables
-    private int scoreValue = 0;
+    private static int scoreValue = 0;
+
+
+    void Start()
+    {
+       if (shouldReset == true)
+        {
+            //reset score back to 0
+            scoreValue = 0;
+        }
+        
+        
+        //update display of score based on numerical value
+        scoreDisplay.text = scoreValue.ToString();
+    }
+
     
     //function to add player's score
     //not automatically called by unity, we need to call it ourselves in our code
